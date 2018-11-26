2bc0e95cf4cf139ff16050bf8bb8d1843d4e7af5
scotidodson <scotidodson@gmail.com>
2018-11-26 15:58:10 -0500

    Done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,16 @@
 import piggy from '../porco.png'
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
+
+	const handleRadio = (event) => {
+		props.greaseFilter(event.target.value)
+	}
+
+	const handleDropdown = (event) => {
+		props.nameWeightFilter(event.target.value)
+	}
+
 	return (
 		<div className="navWrapper">
 			<span className="headerText">Hogwarts</span>
@@ -11,6 +20,27 @@ const Nav = () => {
 				</a>
 			</div>
 			<span className="normalText">A React App for County Fair Hog Fans</span>
+			<div>
+				<label>Filter By:</label>
+				<select onChange={handleDropdown}>
+				  <option value="no filter"></option>
+				  <option value="name">Name</option>
+				  <option value="weight">Weight</option>
+				</select>
+				<div onChange={handleRadio}>
+					<input type="radio"
+			     name="greasy" value="all"/>
+			    <label>All Hogs</label>
+
+			    <input type="radio"
+			     name="greasy" value="greased" />
+			    <label>Greased Hogs</label>
+
+			    <input type="radio" id="contactChoice3"
+			     name="greasy" value="ungreased" />
+			    <label>Ungreased Hogs</label>
+				</div>
+			</div>
 		</div>
 	)
 }
