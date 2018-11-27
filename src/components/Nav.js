f7e9958d91641c0c52d7b68a7c75c5ef1899c59f
TurtleFeeder <t.choong@yahoo.com>
2018-11-27 08:29:27 -0500

    basic deliverables completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import piggy from '../porco.png'
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
 	return (
 		<div className="navWrapper">
 			<span className="headerText">Hogwarts</span>
@@ -11,8 +11,22 @@ const Nav = () => {
 				</a>
 			</div>
 			<span className="normalText">A React App for County Fair Hog Fans</span>
+			<label>Sort By</label>
+			<select onChange={props.handleSort}>
+				<option value=""></option>
+				<option value="name">name</option>
+				<option value="weight">weight</option>
+			</select>
+			<div onChange={props.handleFilter}>
+				<input type="radio" name="grease" value="all" />
+				<label>All Hoggers </label>
+				<input type="radio" name="grease" value="greased" />
+				<label>Greased Hoggers </label>
+				<input type="radio" name="grease" value="not greased" />
+				<label>Un-Greased Hoggers </label>
+			</div>
 		</div>
 	)
-}
+} // end Nav function component
 
 export default Nav
